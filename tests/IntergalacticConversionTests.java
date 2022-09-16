e8cce76f4c9dff140c9ab343e0fd71a50653bf65
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-16 16:00:01 +0200

    Added initial tests for IntergalacticConversion class

---
+++ tests/IntergalacticConversionTests.java
@@ -0,0 +1,87 @@
+import com.esiljak.exceptions.IllegalRomanNumeralException;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntergalacticConversionTests {
+    private Map<String, String> map = Stream.of(new String[][]{
+            { "name1", "I" },
+            { "name2", "V" },
+            { "name3", "X" }
+    }).collect(Collectors.toMap((item) -> item[0], (item) -> item[1]));
+
+    @Test
+    void constructorTest(){
+        IntergalacticConversion conversion = new IntergalacticConversion();
+
+        assertTrue(conversion.getEntries().isEmpty(), "If no value is passed in constructor there should be no entries");
+    }
+
+    @Test
+    void constructorWithParameterTest(){
+        IntergalacticConversion conversion = new IntergalacticConversion(map);
+
+        assertEquals(3, conversion.getEntries().size(), "Entries not properly set through constructor");
+    }
+
+    @Test
+    void setterTest(){
+        IntergalacticConversion conversion = new IntergalacticConversion();
+        conversion.setEntries(map);
+
+        assertEquals("I", conversion.getEntries().get("name1"), "Entries not properly set through setter");
+    }
+
+    @Test
+    void addingPairsTest(){
+        IntergalacticConversion conversion = new IntergalacticConversion();
+        conversion.addEntry("entry1", "X");
+        conversion.addEntry("entry2", "I");
+
+        assertEquals("X", conversion.getEntries().get("entry1"), "Entry not properly added through addEntry");
+        assertEquals("I", conversion.getEntries().get("entry2"), "Entry not properly added through addEntry");
+    }
+
+    @Test
+    void duplicatingKeysTest(){
+        assertThrows(DuplicatedConversionKey.class, () -> {
+            IntergalacticConversion conversion = new IntergalacticConversion();
+            conversion.addEntry("entry1", "X");
+            conversion.addEntry("entry1", "I");
+        }, "Cannot have multiple entries with the same key");
+
+        assertThrows(DuplicatedConversionKey.class, () -> {
+            IntergalacticConversion conversion = new IntergalacticConversion();
+            conversion.addEntry("entry1", "X");
+            conversion.addEntry("Entry1", "I");
+        }, "Keys are case insensitive");
+
+        assertThrows(DuplicatedConversionValue.class, () -> {
+            IntergalacticConversion conversion = new IntergalacticConversion();
+            conversion.addEntry("entry1", "X");
+            conversion.addEntry("entry2", "X");
+        }, "Cannot have multiple entries with the same value");
+    }
+
+    @Test
+    void invalidValueTest(){
+        assertThrows(IllegalRomanNumeralException.class, () -> {
+           IntergalacticConversion conversion = new IntergalacticConversion();
+           conversion.addEntry("entry", "T");
+        });
+
+        assertThrows(IllegalRomanNumeralException.class, () -> {
+            IntergalacticConversion conversion = new IntergalacticConversion();
+            conversion.addEntry("entry", "i");
+        });
+
+        assertThrows(IllegalRomanNumeralException.class, () -> {
+            IntergalacticConversion conversion = new IntergalacticConversion();
+            conversion.addEntry("entry", "XX");
+        });
+    }
+}
